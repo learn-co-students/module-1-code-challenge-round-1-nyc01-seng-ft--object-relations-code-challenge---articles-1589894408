607a6d4e99eb3793720108443f5393907563f2d8
callas4ever <thetseringnorbu@gmail.com>
2020-05-19 10:12:30 -0400

    My second commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,47 @@
 class Article
+# # Article
+ @@all =[]
+ attr_reader :title
+    #  - An article **cannot** change its author, magazine, or title after it is has been initialized.
+    # - `Article#title`
+    #   - Returns the title for that given article
+ def initialize(title, author, magazine)
+    # - `Article#initialize(author, magazine, title)`
+    #   - An article is initialized with an author as an 
+    #Author object, a magazine as a Magazine object, and title as a string.
+    
+    @title=title
+    @author=author
+    @magazine=magazine
+    @@all << self
+ end
+
+ def self.all
+    # - `Article.all`
+    #   - Returns an array of all Article instances
+    @@all
+ end
+ def author
+    #  - `Article#author`
+    #   - Returns the author for that given article
+    all.each do |art|
+        
+        if art==self
+           return art.author
+        end
+    end
+  end
+
+   def magazine
+    # - `Article#magazine`
+    #   - Returns the magazine for that given article
+    all.each do |art|
+        if art==self
+            return art.magazine
+        end
+    end
+end
+
 
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,38 @@
 class Magazine
+  @@all =[]
   attr_accessor :name, :category
+# Magazine
+
+# - `Magazine#initialize(name, category)`
+#   - A magazine is initialized with a name as a string and a category as a string
+#   - The name and category of the magazine **can be** changed after being initialized.
+# - `Magazine#name`
+#   - Returns the name of this magazine
+# - `Magazine#category`
+#   - Returns the category of this magazine
 
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
+  def self.all
+    # - `Magazine.all`
+    #   - Returns an array of all Magazine instances
+    @@all
+  end
+  def contributors
+  # - `Magazine#contributors`
+  # - Returns an array of Author instances who have written for this magazine
+  athr=[]
+  Article.all.each do |art|
+    if art.magazine==self
+      athr << art.author
+    end
+  end
+end
+
 
 
 end

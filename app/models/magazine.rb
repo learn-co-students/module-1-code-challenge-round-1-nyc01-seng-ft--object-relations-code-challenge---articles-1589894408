0845f860fc9903006ed41542871f97b7a6d76abd
CMSTie <cmstie@gmail.com>
2020-05-19 10:35:40 -0400

    third commit

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,4 +1,5 @@
 class Magazine
+  
   attr_accessor :name, :category
 
   @@all = []
@@ -36,6 +37,29 @@ class Magazine
       end
     end
     articles
+  end
+
+  def all_authors
+    all = []
+    Article.all.select do |article|
+      if article.magazine == self
+      all.push(article.author.name)
+      end
+    end
+  end
+
+  def contributing_authors
+    con_arr = []
+    contrib = Hash.new(0)
+    all_authors.map do |article|
+      contrib[article.author.name] +=1
+    end
+    contrib.each do |name, article|
+      if article > 2
+        con_arr.push(name)
+      end
+    end
+      con_arr
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -9,6 +9,8 @@ end
 
 author1 = Author.new("Bob")
 author2 = Author.new("Jeremy")
+author3 = Author.new("Mike")
+author4 = Author.new("Carl")
 
 mag1 = Magazine.new("Car and Driver", "Cars")
 mag2 = Magazine.new("MotorTrend", "Cars")
@@ -21,6 +23,10 @@ art3 = Article.new(author2, mag1, "Porsche")
 art4 = Article.new(author2, mag1, "Chevy")
 art5 = Article.new(author1, mag3, "Boom")
 art6 = Article.new(author1, mag4, "Explosion")
+art7 = Article.new(author2, mag1, "Ford")
+art8 = Article.new(author4, mag1, "Tesla")
+art9 = Article.new(author4, mag1, "Toyota")
+art10 = Article.new(author4, mag1, "Scion")
 
 
 

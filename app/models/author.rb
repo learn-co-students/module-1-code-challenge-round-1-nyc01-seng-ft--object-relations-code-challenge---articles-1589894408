e2ac32bbf6d629e59eabd4e8e35e9bea40a09b22
grantkoji <grantkoji@gmail.com>
2020-05-19 10:11:16 -0400

    draft1 Grant

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,35 @@
 class Author
-  attr_accessor :name
-
+  attr_reader :name
 
   def initialize(name)
     @name = name
   
   end
+
+
+  def articles
+    Article.all.select do |articles|
+      articles.author == self
+    end
+  end
+
+
+  def magazines
+    self.articles.map do |articles|
+      articles.magazine
+    end.uniq
+  end
+
+  def add_article(magazine_instance, title_string)
+    Article.new(self, magazine_instance, title_string)
+  end
+  
+  def topic_areas
+    self.magazines.map do |magazine|
+      magazine.category     
+    end.uniq
+  end
+
 
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,46 @@
 class Magazine
   attr_accessor :name, :category
-
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
+    Magazine.all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select do |articles|
+      articles.magazine == self
+    end
+  end
+
+  def contributors
+    self.articles.map do |articles|
+      articles.author
+    end
+
+  def self.find_by_name(name_string)
+    Magazine.all.find do |magazines|
+      magazines.name == name_string
+    end
+  end
+
+  def article_titles
+    self.articles.map do |article|
+      article.title
+    end
+  end
+
+
+
+  def contributing_authors
+    self.articles.select do |article|
+        article.count(author) > 2
+    end
   end
 
 

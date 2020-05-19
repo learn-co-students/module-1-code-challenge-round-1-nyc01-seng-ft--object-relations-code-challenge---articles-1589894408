65f6951aaf0d8bc60b70eac36ec3f105d8d9c603
kevinneyer <kevinneyer@gmail.com>
2020-05-19 10:11:17 -0400

    second commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,24 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
 
   def initialize(name)
     @name = name
+   end
+
+   def articles
+    Article.all.select do |article_instance|
+      article_instance.author == self 
+    end
+  end
+
+  # def magazines
+  #   Article.all
   
+  # end
+
+  def add_article(magazine, title)
+    New.article(self, magazine, title)
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,10 +7,17 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+jake = Author.new("Jake")
+bob = Author.new("Bob")
 
+people = Magazine.new("People", "Pop Cuture")
+si = Magazine.new("Sports Illustrated", "Sports")
+time = Magazine.new("Time", "Current Events")
 
-
-
+article1 = Article.new(jake, si, "Football")
+article2 = Article.new(bob, people, "Beautiful People")
+article3 = Article.new(jake, si, "Baseball")
+article4 = Article.new(jake, time, "Hockey")
 
 
 

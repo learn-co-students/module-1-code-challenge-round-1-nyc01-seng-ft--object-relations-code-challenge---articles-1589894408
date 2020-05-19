1dfa828e1fc9ea0788013be851897f70534ecea6
smatalha <sma.talha@gmail.com>
2020-05-19 10:11:08 -0400

    My second commit

--- tools/console.rb
+++ tools/console.rb
@@ -7,12 +7,15 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+rachel = Author.new("Rachel Carson")
+jk = Author.new("J. K. Rowling")
+lauren = Author.new("Lauren Duca")
 
+nature = Magazine.new("National Geographic", "Nature")
+culture = Magazine.new("Rolling Stone", "popular culture")
+fashion = Magazine.new("Vogue", "Fashion")
 
-
-
-
-
+article1 = Article.new()
 
 
 ### DO NOT REMOVE THIS

5e77cd994ee56bb6575b9356ff06fda44c5c3e5e
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-05-19 10:25:55 -0400

    my fifth commit

--- tools/console.rb
+++ tools/console.rb
@@ -7,10 +7,17 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author1 = Author.new("Author1")
+author2 = Author.new("Author2")
+author3 = Author.new("Author2")
 
+magazine1 = Magazine.new("Magazine1", "category1")
+magazine2 = Magazine.new("Magazine2", "category2")
+magazine3 = Magazine.new("Magazine3", "category3")
 
-
-
+article1 = Article.new("Title1", author1, magazine1)
+article2 = Article.new("Title2", author2, magazine2)
+article3 = Article.new("Title2", author3, magazine3)
 
 
 

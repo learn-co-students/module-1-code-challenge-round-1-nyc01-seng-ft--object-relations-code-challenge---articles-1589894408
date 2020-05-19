66dad8b19b8053dab0c531795c7a35195f681c90
chloekim917 <chloekim917@gmail.com>
2020-05-19 11:04:59 -0400

    third commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author
   attr_reader :name
-  attr_accessor :article
+  attr_accessor :article, :magazine
   @@all  = []
 
   def initialize(name)
@@ -13,11 +13,22 @@ class Author
   end
 
   def articles
-    Article.all.select {|articles| article.author == self}
+    Article.all.select {|articles| articles.author== self}
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def magazines_not_uniq
+    Magazine.all.select {|magazines| magazines.author == self}
   end
 
   def magazines
-    Magazine.all.select {|magazines| magazine.author == self}
-    .uniq
+    self.magazines_not_uniq.uniq
+  end
+
+  def topic_areas
+    self.magazines_not_uniq.select {|magazine| magazine.category}.uniq
   end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,5 +1,5 @@
 class Magazine
-  attr_accessor :name, :category, :article, :author
+  attr_accessor :name, :category, :author
   @@all = []
 
 
@@ -15,5 +15,22 @@ class Magazine
 
   def contributors
     Author.all.select {|authors| authors.magazine == self}
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|magazines| magazines.name == name}
+  end
+
+  def articles
+    Article.all.select {|articles| articles.magazine == self}
+  end
+
+  def article_titles
+    self.articles.map {|articles| articles.title}
+  end
+
+  def contributing_authors
+    more_than_one = []
+    self.contributors.select {|contributors| contributors == contributors}
   end
 end

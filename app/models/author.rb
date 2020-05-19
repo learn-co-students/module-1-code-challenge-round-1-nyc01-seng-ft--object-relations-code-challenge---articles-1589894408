248e948eedd9632f2afc1e4fda72bba951181aeb
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-19 10:28:47 -0400

    author class and instance methods

--- app/models/author.rb
+++ app/models/author.rb
@@ -7,20 +7,27 @@ class Author
   
   end
 
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    articles.map do |article|
+      article.magazine
+    end
+  end
+
+  def add_article(magazine, title)
+    Article.new(self, magazine, title)
+  end
+
+  def topic_areas
+    magazines.map do |magazine|
+      magazine.category
+    end
+  end
+
 
 end
-
-
-# #### Author
-
-# - `Author#articles`
-#   - Returns an array of Article instances the author has written
-# - `Author#magazines`
-#   - Returns a **unique** array of Magazine instances for which the author has contributed to
-
-# #### Author
-
-# - `Author#add_article(magazine, title)`
-#   - Given a magazine (as Magazine instance) and a title (as a string), creates a new Article instance and associates it with that author and that magazine.
-# - `Author#topic_areas`
-#   - Returns a **unique** array of strings with the categories of the magazines the author has contributed to

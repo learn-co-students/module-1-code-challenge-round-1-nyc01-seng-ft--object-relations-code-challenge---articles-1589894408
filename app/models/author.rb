f08d25098a271caf24785e7057fdea2c370319da
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-05-19 10:11:19 -0400

    second commit

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author
-  attr_accessor :name
 
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -9,3 +9,18 @@ class Author
 
 
 end
+
+
+# #### Author
+
+# - `Author#articles`
+#   - Returns an array of Article instances the author has written
+# - `Author#magazines`
+#   - Returns a **unique** array of Magazine instances for which the author has contributed to
+
+# #### Author
+
+# - `Author#add_article(magazine, title)`
+#   - Given a magazine (as Magazine instance) and a title (as a string), creates a new Article instance and associates it with that author and that magazine.
+# - `Author#topic_areas`
+#   - Returns a **unique** array of strings with the categories of the magazines the author has contributed to
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,27 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+author1 = Author.new("author one")
+author2 = Author.new("author two")
+author3 = Author.new("author three")
+author4 = Author.new("author four")
+author5 = Author.new("author five")
 
+magazine1 = Magazine.new("magazine1", "category1")
+magazine2 = Magazine.new("magazine2", "category1")
+magazine3 = Magazine.new("magazine3", "category2")
+magazine4 = Magazine.new("magazine4", "category2")
+magazine5 = Magazine.new("magazine5", "category3")
+
+article1 = Article.new(author1, magazine1, "Article One")
+article2 = Article.new(author1, magazine1, "Article Two")
+article3 = Article.new(author1, magazine1, "Article Three")
+article4 = Article.new(author1, magazine1, "Article Four")
+article5 = Article.new(author1, magazine1, "Article Five")
+article6 = Article.new(author1, magazine1, "Article Six")
+article7 = Article.new(author1, magazine1, "Article Seven")
+article8 = Article.new(author1, magazine1, "Article Eight")
+article9 = Article.new(author1, magazine1, "Article Nine")
 
 
 
